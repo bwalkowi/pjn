95c5a915bbaa86618425e11be97da9136a4f4e6e
Bartosz Walkowicz <wallko1@hotmail.com>
2018-05-15 08:19:21 +0200

    ex8 finish histograms

---
+++ pjn_8/main.py
@@ -0,0 +1,96 @@
+import os
+import pickle
+import xml.etree.ElementTree
+from collections import Counter
+
+import matplotlib.pyplot as plt
+
+
+INPUT_DIR = './judgments'
+PICKLE_FILE = './categories.pickle'
+
+
+def read_xmls():
+    categories = {}
+
+    for file in os.listdir(INPUT_DIR):
+        with open(os.path.join(INPUT_DIR, file)) as f:
+            text_xml = xml.etree.ElementTree.fromstring(f.read())
+
+        for item in text_xml.iter():
+            if item.tag == 'tok':
+                content = ''
+                for child in item.getchildren():
+                    if child.tag == 'orth':
+                        content = child.text
+                    if child.tag == 'ann' and int(child.text) >= 1:
+                        minor_category = child.attrib['chan']
+                        major_category = "_".join(minor_category.split('_')[:2])
+
+                        minor_categories = categories.get(major_category, {})
+                        minor_category_counter = minor_categories.get(minor_category, Counter())
+                        minor_category_counter.update([content])
+
+                        minor_categories[minor_category] = minor_category_counter
+                        categories[major_category] = minor_categories
+
+    with open(PICKLE_FILE, 'wb') as f:
+        pickle.dump(categories, f)
+
+    return categories
+
+
+def numbers():
+    num = 0
+    while True:
+        yield num
+        num += 1
+
+
+def histograms(categories, merge_minor=False):
+    ids = numbers()
+    y_pos = []
+    labels = []
+    data = []
+    for major_cat, minor_categories in categories.items():
+        if merge_minor:
+            y_pos.append(next(ids))
+            labels.append(major_cat)
+            quantity = 0
+            for minor_counts in minor_categories.values():
+                quantity += sum(minor_counts.values())
+            data.append(quantity)
+        else:
+            for minor_cat, counts in minor_categories.items():
+                y_pos.append(next(ids))
+                labels.append(minor_cat)
+                data.append(sum(counts.values()))
+
+    for label, val in zip(labels, data):
+        print(f'{label}: {val}')
+
+    plt.bar(y_pos, data, align='center', alpha=0.5)
+    if not merge_minor:
+        plt.xticks(y_pos, labels, rotation='vertical')
+    else:
+        plt.xticks(y_pos, labels)
+    plt.xlabel('Categories names')
+    plt.ylabel('Quantity of each category')
+    plt.title(f'{"Major" if merge_minor else "Minor"} categories')
+    plt.tight_layout()
+    plt.show()
+
+
+def main():
+    if os.path.isfile(PICKLE_FILE):
+        with open(PICKLE_FILE, 'rb') as f:
+            categories = pickle.load(f)
+    else:
+        categories = read_xmls()
+
+    histograms(categories)
+    histograms(categories, merge_minor=True)
+
+
+if __name__ == '__main__':
+    main()
---
+++ pjn_8/readme.md
@@ -0,0 +1,56 @@
+## Histogram dla podkategorii
+
+```
+nam_org_institution: 4619
+nam_org_company: 2862
+nam_org_organization: 382
+nam_org_group_team: 48
+nam_org_political_party: 1
+nam_org_nation: 7
+nam_liv_person: 2328
+nam_liv_god: 1
+nam_loc_gpe_city: 522
+nam_loc_gpe_admin1: 4
+nam_loc_gpe_country: 28
+nam_loc_gpe_admin3: 12
+nam_loc_historical_region: 1
+nam_fac_road: 205
+nam_fac_goe: 71
+nam_num_house: 131
+nam_oth_currency: 260
+nam_oth_tech: 35
+nam_oth_other: 6
+nam_oth_position: 9
+nam_eve_human: 20
+nam_pro_media_periodic: 897
+nam_pro_title_document: 1618
+nam_pro_software: 3
+nam_pro_brand: 60
+nam_pro_title_tv: 2
+nam_pro_title_album: 4
+nam_pro_model_car: 3
+nam_pro_title: 19
+nam_pro_media_web: 1
+nam_adj_country: 4
+nam_adj_other: 1
+```
+
+![Histogram w pliku hist.png](./minor_hist.png)
+
+
+## Histogram dla kategorii
+
+```
+nam_org: 7919
+nam_liv: 2329
+nam_loc: 567
+nam_fac: 276
+nam_num: 131
+nam_oth: 310
+nam_eve: 20
+nam_pro: 2607
+nam_adj: 5
+```
+
+![Histogram w pliku hist.png](./major_hist.png)
+
